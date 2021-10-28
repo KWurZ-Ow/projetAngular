ef35dd12900c5af13fbc1e3bf60fab17ff44a63c
KWurZ-Ow <marius.proton@gmail.com>
2021-10-28 16:38:51 +0200

    amelioration graphique 1

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -3,18 +3,31 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ImagesSingleComponent } from './images-single/images-single.component';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { CharLimitPipe } from '../pipes/char-limit.pipe';
+
+
+
  
 @NgModule({
   declarations: [
-    
-    ImagesSingleComponent
+    ImagesSingleComponent,
+    CharLimitPipe
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
-    ImagesSingleComponent
+    ImagesSingleComponent,
+    CharLimitPipe
   ]
 })
 export class SharedModule {
--- src/app/pages/images/images.component.ts
+++ src/app/pages/images/images.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import * as faker from "faker";
 
 @Component({
   selector: 'app-images',
   templateUrl: './images.component.html',
   styleUrls: ['./images.component.css']
 })
-export class ImagesComponent implements OnInit {
 
+export class ImagesComponent implements OnInit {
+  
   constructor() { }
+  
+  data = [
+    {
+      username: faker.internet.userName(),
+      location: faker.address.cityName(),
+      avatar: faker.image.avatar(),
+      image: faker.image.imageUrl( 332, 249, undefined, true, true),
+      likes: faker.datatype.number({'min': 0,'max': 15}),
+      description: faker.lorem.paragraph(1)
+    },
+    {
+      username: faker.internet.userName(),
+      location: faker.address.cityName(),
+      avatar: faker.image.avatar(),
+      image: faker.image.imageUrl( 332, 249, undefined, true, true),
+      likes: faker.datatype.number({'min': 0,'max': 15}),
+      description: faker.lorem.paragraph(1)
+    },
+    {
+      username: faker.internet.userName(),
+      location: faker.address.cityName(),
+      avatar: faker.image.avatar(),
+      image: faker.image.imageUrl( 332, 249, undefined, true, true),
+      likes: faker.datatype.number({'min': 0,'max': 15}),
+      description: faker.lorem.paragraph(1)
+    },
+    {
+      username: faker.internet.userName(),
+      location: faker.address.cityName(),
+      avatar: faker.image.avatar(),
+      image: faker.image.imageUrl( 332, 249, undefined, true, true),
+      likes: faker.datatype.number({'min': 0,'max': 15}),
+      description: faker.lorem.paragraph(1)
+    },
+    {
+      username: faker.internet.userName(),
+      location: faker.address.cityName(),
+      avatar: faker.image.avatar(),
+      image: faker.image.imageUrl( 332, 249, undefined, true, true),
+      likes: faker.datatype.number({'min': 0,'max': 15}),
+      description: faker.lorem.paragraph(1)
+    },
+    {
+      username: faker.internet.userName(),
+      location: faker.address.cityName(),
+      avatar: faker.image.avatar(),
+      image: faker.image.imageUrl( 332, 249, undefined, true, true),
+      likes: faker.datatype.number({'min': 0,'max': 15}),
+      description: faker.lorem.paragraph(1)
+    }
+  ]
 
   ngOnInit(): void {
   }
-
+  
 }
