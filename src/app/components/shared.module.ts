00ede518d4af0990eebcbd356330a7058a6026da
KWurZ-Ow <marius.proton@gmail.com>
2021-10-28 13:10:07 +0200

    premiers composants

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -2,15 +2,19 @@ import {NgModule} from '@angular/core';
 
 import {CommonModule} from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ImagesSingleComponent } from './images-single/images-single.component';
  
 @NgModule({
   declarations: [
+    
+    ImagesSingleComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
   exports: [
+    ImagesSingleComponent
   ]
 })
 export class SharedModule {
